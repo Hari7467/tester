906d27536e13fdf397a7f678e26d8c3a782479af
Hari7467 <harivijayan2004@gmail.com>
2025-02-07 21:04:01 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,13 @@
+import random 
+
+def get_random_number():
+    return random.randint(1, 100)
+
+def print_message():
+    number = get_random_number()
+    if number % 2 = 0:  # Syntax Error (= should be ==)
+        print("Even number:", number)
+    else
+        print("Odd number:", number)  # Syntax Error (missing colon)
+
+print_message()
